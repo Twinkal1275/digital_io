ba8f17d2633b9d88f53990066723aa7512659c75
TWIN_PATEL <140789119+Twinkal1275@users.noreply.github.com>
2024-05-21 14:39:47 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// document.getElementById("InputForm").addEventListener("submit",function(event){
+//     event.preventDefault();
+
+
+        
+//         // for value
+//         var name = document.getElementById("name").value;
+//         var email = document.getElementById("mail").value;
+//         var city = document.getElementById("city").value;
+
+//         popupcontact.innerHTML = `Name: ${name}<br>Email: ${email}<br>Age: ${age}`;
+//         popup.style.display = 'block';
+
+//         // alert
+//         alert("Your data has been successfully stored!");
+
+//         // reset
+//         document.getElementById("userDataForm").reset();
+        
+// })
+
+// const form = document.getElementById('InputForm');
+// const popup = document.getElementById('popup');
+// const popupcontact = document.getElementById('popupcontact');
+// const closebtn = document.getElementById('closebtn');
+
+// form.addEventListener('submit', function(event){
+//   event.preventDefault();
+
+//   const name = document.getElementById('name').value;
+//   const email = document.getElementById('mail').value;
+//   const age = document.getElementById('age').value;
+
+//   popupcontact.innerHTML = `Name: ${name}<br>Email: ${email}<br>Age: ${age}`;
+//   popup.style.display = 'block';
+// });
+
+// closebtn.addEventListener('click', function(){
+//     popup.style.display = 'none';
+// });
